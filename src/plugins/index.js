c54f1a85e35af94a56d6fdb5ca536daaf3769997
chenkai0520 <k_0520@qq.com>
2021-12-16 17:37:40 +0800

    fix: del naive-ui

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,11 +1,9 @@
 import ElementPlus from '@/plugins/element-plus';
-import NaiveUi from '@/plugins/naive-ui';
 import GlobalProperties from '@/plugins/global-properties';
 
 export default {
   install(app) {
     app.use(ElementPlus);
-    app.use(NaiveUi);
     app.use(GlobalProperties);
   },
 };
